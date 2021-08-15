9b1040c08b7f06e1f12a75a2049bc2205b00bf20
Skeletrox <android.svr@gmail.com>
2021-08-14 23:13:29 -0700

    player boilerplate code

--- player/models.py
+++ player/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
+
+SECRET_POLICE_RANKS = [
+    "Sergeant",
+    "Lieutenant",
+    "Major",
+    "Lieutenant Colonel",
+    "Colonel",
+    "Commissioner General",
+    "General Secretary"
+]
+
 # Create your models here.
+class Player(models.Model):
+    # TODO: replace alL external model references with actual models
+    name = models.CharField(max_length=64)
+    owned_cash = models.IntegerField(default=0)
+    owned_properties = []
+
+    # TODO: add behavior to methods
+
+    # roll a 6-sided die
+    def roll_die(self):
+        pass
+
+    # purchase a property
+    def purchase_property(self, property):
+        pass
+
+    # modify property
+    def modify_property(self, property, action):
+        pass
+
+
+class SecretPolice(Player):
+    rank = models.IntegerField(default=1)
+
+    # promote player
+    def promote(self):
+        pass
+
+    # demote player
+    def demote(self):
+        pass
+
+
+class Citizen(Player):
+    is_hero = models.BooleanField(default=False)
